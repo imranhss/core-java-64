4a94509d5d1ec49ac34382ba51cb3ab10527e9e2
Muhammad Emran Hossain <emranhss@gmail.com>
2025-03-24 14:31:01 +0600

    Add

---
+++ TestDataType/src/testdatatype/TestDataType.java
@@ -0,0 +1,28 @@
+
+package testdatatype;
+
+
+public class TestDataType {
+
+  
+    public static void main(String[] args) {
+        
+        byte id=120;
+        short roll=45;
+        int number=564564645;
+        long longNumber=4156456456464654445l;
+        
+        float price=1520.25f; 
+        double doublePrice=1520.25; 
+        
+        char latter='\u0041';
+        String name="Rahim";
+        boolean status=true;
+        
+        System.out.println("The latter is "+latter);
+        
+        
+        
+    }
+    
+}
